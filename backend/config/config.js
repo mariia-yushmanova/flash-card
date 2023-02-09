e07b942f9057a1849621ac82f2c62ac307add20e
Mariia <pravdova97@mail.ru>
2023-02-09 21:22:12 +0300

    fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const { resLocals, getUser } = require('../middleware/auth');
-const cors = require('cors');
 const sessionConfig = require('./session');
 
 const corsOptions = {
@@ -16,12 +16,12 @@ const serverConfig = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static('public'));
+  app.use(cors(corsOptions));
   app.use(session(sessionConfig));
   app.use(cookieParser());
   app.disable('x-powered-by');
   app.use(resLocals);
   app.use(getUser);
-  app.use(cors(corsOptions));
 };
 
 module.exports = serverConfig;
