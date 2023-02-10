9280360b57d5dfda5261ad1da3f82e34d451f9a9
PenRomain <romandreevich@icloud.com>
2023-02-10 13:00:42 +0300

    disabled actions on the cards when you will answer

--- frontend/src/features/Auth/Types/Action.ts
+++ frontend/src/features/Auth/Types/Action.ts
@@ -1,9 +1,8 @@
-// import { type } from 'os';
 import { State } from './types';
 
 type Action =
-  { type: 'REG_USER';payload: State; }
-| { type: 'LOGOUT' }
-| { type: 'LOG_USER';payload: State };
+  | { type: 'REG_USER'; payload: State }
+  | { type: 'LOGOUT' }
+  | { type: 'LOG_USER'; payload: State };
 
 export default Action;
